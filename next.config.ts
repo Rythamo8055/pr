e38bdd6b3d699179446bcf5384303b3032695b9c
Rythamo8055 <rythamo8055@gmail.com>
2025-06-16 23:10:03 +0530

    Configure for Cloudflare Pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,33 @@
 import type {NextConfig} from 'next';
 
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**', 
+      },
+    ],
+  },
+};
+
+if (process.env.NODE_ENV === 'development') {
+  await import('@cloudflare/next-on-pages/next-dev').then(({ setupDevPlatform }) => setupDevPlatform());
+}
+
+
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
